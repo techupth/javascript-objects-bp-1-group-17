5ba0a9d04dc70a258010c16f86ea431ace7ce991
mosigaaa <mosiga2008@gmail.com>
2023-09-21 14:46:53 +0700

    "Feat: Complete Assignment by Moss"

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,10 @@
 // Exercise #1: Food Order
-
 // Start coding here
+let foodOrder = {
+    orderNumber : "A0234",
+    address : "Bangkok",
+    paymentType : "Cash",
+    totalPrice : 4500,
+    restaurantName : "MK"
+}
+console.log(foodOrder)
